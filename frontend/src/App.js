89e1722d2b6a96370f4a5907bc32187f8b54945e
Shivam-Kansagara <shivamkansagara1@gmail.com>
2024-10-29 20:29:01 +0530

    feat : footer and header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "react-bootstrap";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header></Header>
+      <main className="py-3">
+        <Container>
+          <h1>App</h1>
+        </Container>
+      </main>
+      <Footer></Footer>
+    </>
+  );
 };
 
 export default App;
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,30 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import logo from "../assets/logo.png";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">
+            <img src={logo} alt="Pro Shop"></img> ProShop
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse>
+            <Nav className="ms-auto">
+              <Nav.Link href="/cart">
+                <FaShoppingCart></FaShoppingCart> Cart
+              </Nav.Link>
+              <Nav.Link href="/cart">
+                <FaUser></FaUser> Sign In
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
